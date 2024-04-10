f7274d09be27c39abb20218c2d6ef54672ddc84c
Trey Turner <trey.turner1857@gmail.com>
2024-04-09 22:59:13 -0500

    fixed whitespace in both files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,27 +7,27 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0;
-    size_t index;
-    size_t tmp = 0;
+	size_t i = 0;
+	size_t index;
+	size_t tmp = 0;
 
-    if (size < 2)
-    {
-        return;
-    }
+	if (size < 2)
+	{
+		return;
+	}
 
-    while (i < size)
-    {
-        for (index = 0; index < size; index++)
-        {
-            if (array[index] > array[index + 1] && array[index + 1])
-            {
-                tmp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-        i++;
-    }
+	while (i < size)
+	{
+		for (index = 0; index < size; index++)
+		{
+			if (array[index] > array[index + 1] && array[index + 1])
+			{
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
