92ad06589504a978f0767082689b5495e58b2ba8
Trey Turner <trey.turner1857@gmail.com>
2024-04-09 23:28:59 -0500

    created the selection sort function

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array in ascending order
+ * @array: array to sort
+ * @size: size of array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
+	{
+	return;
+	}
+
+	while (i < size)
+	{
+		tmp = i;
+		flag = 0;
+
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+
+		if (flag != 0)
+		{
+			print_array(array, size);
+		}
+		i++;
+	}
+}
