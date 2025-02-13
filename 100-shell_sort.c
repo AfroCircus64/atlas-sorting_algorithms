66394927e6d2443ebc4732a3915972ea45c64b63
Trey Turner <trey.turner1857@gmail.com>
2025-02-12 22:39:36 -0600

    Shell sort complete (Tested)

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,5 +9,33 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	/* Code goes here */
+	size_t gap = 1;
+	size_t i, j;
+	int tmp;
+
+	if (!array || size < 2)
+	{
+		return;
+	}
+
+	while (gap < size / 3)
+	{
+		gap = gap * 3 + 1;
+	}
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = tmp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
